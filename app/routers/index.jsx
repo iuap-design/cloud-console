de06d16e35334d674fafd13dd41f193d1618a5ef
guoyff <guoyff@yonyou.com>
2017-01-12 22:59:11 +0800

    fix bug

--- app/routers/index.jsx
+++ app/routers/index.jsx
@@ -1,13 +1,13 @@
 import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import React from 'react'
 
-import Layout from '../layouts/Layout.jsx'
+import ConsoleLayout from '../layouts/ConsoleLayout'
 import Home from '../views/Home.jsx'
 import DeployList from '../views/DeployList.jsx'
 
 export default (
   <Router history={hashHistory}>
-    <Route path="/" component={Layout}>
+    <Route path="/" component={ConsoleLayout}>
       <IndexRoute component={Home} />
       <Route path="list" component={DeployList} />
     </Route>
