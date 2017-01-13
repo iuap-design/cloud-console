c8ec87aaa8946ac8a7380ed4b39591191f9a1e2e
guoyff <guoyff@yonyou.com>
2017-01-13 16:24:22 +0800

    新增redux和路由的配置

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,9 +1,9 @@
-import { combineReducer } from 'redux'
+import { combineReducers } from 'redux'
 
-import AppReducer from '../containers/App/reducer.js'
+import List from '../containers/List/reducer.js'
 
 // 将每个组件的 reducer 都导入并 combine 合并后输出
 // 在创建 store 的时候引入使用
-export default combineReducer({
-  AppReducer
+export default combineReducers({
+  List
 })
--- app/views/Home.jsx
+++ app/views/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import List from '../containers/List'
+
 export default ( props ) => (
   <div>
+    <List />
     <Link to="/list"> 我是首页，点我跳转到部署列表页面</Link>
   </div>
 )
